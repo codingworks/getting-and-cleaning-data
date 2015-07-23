dc1f9de32ed6c1f8b21f7faa425e4127106bfaa5
Rongqin Sheng <rongqinsheng@yahoo.com>
2015-07-23 11:35:16 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -22,18 +22,21 @@ data_step1 <-  cbind(labels, subjects, data)
 
 # Step 2
 
-# Load data frame features and select rows for mean and standard deviation only
+# Load features and select rows for mean and standard deviation only
 features <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
 names(features) <- c("feat_id", "feat_desc")
-feat_selected <- filter(features, grepl("mean\\(\\)", feat_desc) | grepl("std\\(\\)",feat_desc))
+feat_selected <- filter(features, grepl("mean\\(\\)", feat_desc) | 
+    grepl("std\\(\\)", feat_desc))
 
-# Extract only the measurements on the mean and standard deviation for each measurement
-data_step2 <- data_step1[,c(1,2,feat_selected$feat_id + 2)]
+# Extract only the measurements on the mean and standard deviation 
+# for each measurement
+data_step2 <- data_step1[, c(1, 2, feat_selected$feat_id + 2)]
 
 # Step 3
 
 # Load activity labels
-activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFactors = FALSE)
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", 
+    stringsAsFactors = FALSE)
 
 # Use descriptive activity names to name the activities in the data set
 names(activity_labels)[1] <- "label"
@@ -46,18 +49,20 @@ data_step3 <- merge(activity_labels, data_step2, by.x = "label", by.y = "label")
 data_step4 <- data_step3[, -1]
 
 # Set descriptive variable names for the data set
-names(data_step4) <- c("activity", "subject", feat_selected$feat_desc)
+meas_col_names <-  gsub("-", "", gsub("std\\(\\)", "Std", (
+    gsub("mean\\(\\)", "Mean", feat_selected$feat_desc))))
+names(data_step4) <- c("activity", "subject", meas_col_names)
 
 # Step 5
 
 # Based on data_step4, create a second, independent tidy data set 
 # with the average of each variable for each activity and each subject
-data_step5 <- group_by(data_step4, activity, subject) %>% summarise_each(funs(mean))
+data_step5 <- group_by(data_step4, activity, subject) %>% 
+    summarise_each(funs(mean))
 
-# Set descriptive variable names for the measurement columns (add a prefix "average-")
+# Set descriptive variable names for measurement columns (add a prefix "avg")
 nc <- ncol(data_step5)
-meas_col_names <- paste("average", names(data_step5)[3:nc], sep = "-")
-names(data_step5)[3:nc] <- meas_col_names
+names(data_step5)[3:nc] <- paste("avg", meas_col_names, sep = "")
 
 # Write data set data_step5 into a file
 write.table(data_step5, "data_step5.txt", row.name=FALSE)
